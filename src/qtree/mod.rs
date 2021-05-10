7cb98bb9d6992ac81e1b223f9b4e675fd59d4d8a
Ivan Xu <info@chivalrystudio.com>
2021-05-09 22:06:07 -0700

    save

--- src/qtree/mod.rs
+++ src/qtree/mod.rs
@@ -6,5 +6,5 @@ pub struct QuadTree<'a, T> {
     max_depth: uint,
     bounds: Bounds,
     elements: Vec<&'a T>,
-    children: Option<[Box<QuadTree<'a, T>>, .. 4]>,
+    children: Option<[Box<QuadTree<'a, T>>; 4]>,
 }
